5f6f34b67a1353a43f3aacf969534e836764cb5b
chouaali <chouaali@gmail.com>
2019-06-08 01:51:07 +0200

    mon premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book/book-list/book-list.component';
+import { BookDetailComponent } from './book/book-detail/book-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './services/auth/auth.component';
+import { BooksComponent } from './services/books/books.component';
+import { AuthGuardComponent } from './services/auth-guard/auth-guard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    BookListComponent,
+    BookDetailComponent,
+    HeaderComponent,
+    AuthComponent,
+    BooksComponent,
+    AuthGuardComponent
   ],
   imports: [
     BrowserModule,
